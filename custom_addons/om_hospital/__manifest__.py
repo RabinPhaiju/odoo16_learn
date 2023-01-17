4dede8c7d08ecac0f78368529d582a5edb609083
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-01-17 17:43:27 +0545

    added hospital addon

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+
+'name': "Hospital Management System",
+'version': '1.0',
+'category': 'Healthcare',
+'summary': 'A module for managing patients, appointments, and billing in a hospital.',
+'description': '''
+        This module provides functionality for managing patients, scheduling appointments, and handling billing in a hospital setting.
+    ''',
+'author': 'Rabin Phaiju',
+'website': 'http://www.rabinphaiju15@gmail.com',
+'category': 'Tools',
+'depends':['base','contacts','hr','account'],
+'data': [
+    'security/ir.model.access.csv',
+    'views/menu.xml',
+    'views/patient.xml',
+],
+'demo':[],
+'images':['static/description/icon.png'],
+'installable': True,
+'application': True,
+'auto_install': False
+}
