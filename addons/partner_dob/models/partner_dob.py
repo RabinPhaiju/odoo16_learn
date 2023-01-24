c8672aa37fbefb9539106cbc7ecff7f794806004
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-01-24 22:46:42 +0545

    added error message in partner_dob

--- addons/partner_dob/models/partner_dob.py
+++ addons/partner_dob/models/partner_dob.py
@@ -1,5 +1,5 @@
 from odoo import api,fields,models,_
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError,UserError,AccessError, MissingError
 
 class PartnerDob(models.Model):
     _inherit = 'res.partner'
@@ -14,3 +14,10 @@ class PartnerDob(models.Model):
             self.is_child = True
         else:
             self.is_child = False
+
+    @api.model_create_multi
+    def create(self,vals_list):
+        for vals in vals_list:
+           if not vals.get('age'):
+                raise ValidationError(_('Age field cannot be empty.'))
+        return super(PartnerDob,self).create(vals_list)
