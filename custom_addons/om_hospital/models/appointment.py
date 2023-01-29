981d70c8cafc7f3a0ea16769fb4d5c1f55e121b0
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-01-29 20:29:49 +0545

    added related field in hospital.appointment

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -7,3 +7,9 @@ class HospitalAppointment(models.Model):
     _description = "Patient Appointment"
 
     patient_id = fields.Many2one('hospital.patient',String="Patient",tracking=True)
+    appointment_time = fields.Datetime(string="Appointment Time",default=fields.Datetime.now,tracking=True)    
+    booking_date = fields.Date(string="Booking Date",default=fields.Date.today,tracking=True)
+    gender = fields.Selection(string="Gender",related='patient_id.gender',readonly=False)
+    age = fields.Integer(string="Age",related='patient_id.age',readonly=False)
+     # suggestion are ignored in related
+     # changing the related value will reflect to the related_field_model_value
