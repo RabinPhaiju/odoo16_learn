46a0f1e6a5392f87c8d8005ca28afd15ed399508
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-02-03 10:18:22 +0545

    res.partner data from website in test.crm.lead

--- addons/crm_test/controllers/main.py
+++ addons/crm_test/controllers/main.py
@@ -12,13 +12,21 @@ class LeadData(http.Controller):
           
           return request.render('crm_test.tmp_leads_data',values)
 
-     @http.route('/crm/leads/create',type='http',auth="user",website=True)
+     @http.route('/crm/leads_form',type='http',auth="user",website=True)
      def leads_form(self,**kw):
           return http.request.render('crm_test.create_lead',{})
 
 
-     @http.route('/create/crm_leads',type='http',auth="user",website=True)
-     def create_crm_lead(self,**kw):
-          print('------------------------------------',kw)
-          # request.env['crm.lead'].sudo().create(kw)
-          return request.render("crm_test.leads_entry",{})
+     @http.route('/create/web_lead',type='http',auth="user",website=True)
+     def create_web_lead(self,**kw):
+          val={
+               'name':kw.get('name'),
+               'age':9, #calculate
+               'email':kw.get('email')}
+          request.env['res.partner'].sudo().create(val)
+          # res.partner ma add vayo
+
+          # aba kasari crm.lead ma res.partner ko relation rakhne
+
+
+          return request.render("crm_test.lead_thanks",{})
