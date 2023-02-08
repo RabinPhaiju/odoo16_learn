566aecc374da31dfecdc35fd66a18d3cb61d09a4
Rabin Phaiju <rabinphaiju15@gmail.com>
2023-02-08 23:47:01 +0545

    inherit action_confirm om_sales

--- addons/om_sales/models/sale_order.py
+++ addons/om_sales/models/sale_order.py
@@ -5,3 +5,8 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     confirmed_user_id = fields.Many2one('res.users',string="Confirmed Users")
+
+    def action_confirm(self):
+        self.confirmed_user_id = self.env.user.id
+        print('---------------- success')
+        super(SaleOrder,self).action_confirm()
